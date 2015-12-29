84c7a79ded5c84718b7c505007ad8fe6f7cf9d39
apple <zhengchen@youzan.com>
2015-12-29 23:06:32 +0800

    添加Executor，订单发货后变成交易完成的延时任务

---
+++ timeout-service/src/main/java/com/youzan/trade/timeout/executor/impl/OrderFinishExecutorImpl.java
@@ -0,0 +1,21 @@
+package com.youzan.trade.timeout.executor.impl;
+
+import com.youzan.trade.timeout.model.DelayTask;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 订单发货后变成交易完成的延时任务
+ *
+ * @author apple created at: 15/12/29 下午11:02
+ */
+@Component
+public class OrderFinishExecutorImpl extends AbstractExecutor {
+
+  @Override
+  protected List<DelayTask> getTaskList() {
+    return null;
+  }
+}
