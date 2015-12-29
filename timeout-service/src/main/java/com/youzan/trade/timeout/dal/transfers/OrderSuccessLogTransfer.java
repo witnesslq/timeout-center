8739f3a4d1513756d03160a2e4a0502adfaa2587
liwenjia <liwenjia@youzan.com>
2015-12-29 16:43:48 +0800

    rename

---
+++ timeout-service/src/main/java/com/youzan/trade/timeout/dal/transfers/OrderSuccessLogTransfer.java
@@ -0,0 +1,19 @@
+package com.youzan.trade.timeout.dal.transfers;
+
+import com.youzan.trade.timeout.dal.dataobject.OrderSuccessDO;
+import com.youzan.trade.timeout.entities.OrderSuccessLog;
+
+/**
+ * @author Created by liwenjia@youzan.com on 2015/12/29 .
+ */
+public class OrderSuccessLogTransfer {
+
+  public static OrderSuccessDO to2do(OrderSuccessLog orderSuccessLog){
+    return null;
+  }
+
+  public static OrderSuccessLog do2to(OrderSuccessDO orderSuccessDO){
+    return null;
+  }
+}
+
