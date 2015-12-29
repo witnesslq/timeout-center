71f5c7d8397e5ca72b54b296997f8b2bed41b1d0
apple <zhengchen@youzan.com>
2015-12-30 00:24:44 +0800

    订单已发货Executor的获取任务的实现

--- timeout-service/src/main/java/com/youzan/trade/timeout/executor/impl/OrderFinishExecutorImpl.java
+++ timeout-service/src/main/java/com/youzan/trade/timeout/executor/impl/OrderFinishExecutorImpl.java
@@ -1,11 +1,15 @@
 package com.youzan.trade.timeout.executor.impl;
 
+import com.youzan.trade.timeout.constants.BizType;
 import com.youzan.trade.timeout.model.DelayTask;
+import com.youzan.trade.timeout.service.DelayTaskService;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
+import javax.annotation.Resource;
 
 /**
  * 订单发货后变成交易完成的延时任务
@@ -18,8 +22,11 @@ public class OrderFinishExecutorImpl extends AbstractExecutor {
   @Value("${order.finish.scan.once.max.size}")
   private int maxSize;
 
+  @Resource
+  private DelayTaskService delayTaskService;
+
   @Override
   protected List<DelayTask> getTaskList() {
-    return null;
+    return delayTaskService.getListWithBizTypeAndTimeoutCurrently(BizType.OrderDelivered.code(), maxSize);
   }
 }
