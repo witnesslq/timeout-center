1a41f713004cbc49e8fa8b7120be508c7a1f1e0c
liwenjia <liwenjia@youzan.com>
2015-12-29 11:27:42 +0800

    add test

---
+++ timeout-service/src/test/java/com/youzan/trade/timeout/TestMainEntry.java
@@ -0,0 +1,14 @@
+package com.youzan.trade.timeout;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Created by liwenjia@youzan.com on 2015/12/29 .
+ */
+public class TestMainEntry {
+
+  public static void main(String[] args) {
+    new ClassPathXmlApplicationContext("classpath:test-applicationContext.xml");
+  }
+
+}
