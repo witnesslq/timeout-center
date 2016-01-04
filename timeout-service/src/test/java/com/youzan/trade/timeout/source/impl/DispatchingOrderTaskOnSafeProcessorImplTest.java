32ba3af8d28d632a9fa1bb7fb096a5cc049ef2aa
liwenjia <liwenjia@youzan.com>
2016-01-04 11:33:28 +0800

    add dispatchingOrderTaskSafeProcessor tc

--- timeout-service/src/test/java/com/youzan/trade/timeout/source/impl/DispatchingOrderTaskOnSafeProcessorImplTest.java
+++ timeout-service/src/test/java/com/youzan/trade/timeout/source/impl/DispatchingOrderTaskOnSafeProcessorImplTest.java
@@ -1,8 +1,11 @@
 package com.youzan.trade.timeout.source.impl;
 
 import com.youzan.trade.timeout.constants.SafeState;
+import com.youzan.trade.timeout.constants.TaskStatus;
 import com.youzan.trade.timeout.model.Safe;
 import com.youzan.trade.timeout.service.SafeService;
+
+import com.google.common.collect.Maps;
 
 import junit.framework.TestCase;
 
@@ -16,6 +19,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -42,7 +47,7 @@ public class DispatchingOrderTaskOnSafeProcessorImplTest extends TestCase {
   }
 
   @Test
-  public void testInferOrderTaskStatusOnSafe() throws Exception {
+  public void testInferOrderTaskStatusOnSafeOrderUnfree() throws Exception {
     //TODO
     String orderNo = "E123";
     Integer kdtId= 1;
@@ -50,6 +55,59 @@ public class DispatchingOrderTaskOnSafeProcessorImplTest extends TestCase {
     safe.setOrderNo(orderNo);
     safe.setKdtId(kdtId);
     when(safeService.checkOrderFeedbackFinish(orderNo, kdtId)).thenReturn(Boolean.FALSE);
-    dispatchingOrderTaskOnSafeProcessor.inferOrderTaskStatusOnSafe(safe);
+    Map<Integer,TaskStatus> datas = inferOrderTaskStatusOnSafeTestDataWithOrderSafeUnFree();
+    for(Map.Entry<Integer,TaskStatus> entry:datas.entrySet()){
+      safe.setState(entry.getKey());
+      assertEquals(entry.getValue(),
+                   dispatchingOrderTaskOnSafeProcessor.inferOrderTaskStatusOnSafe(safe));
+    }
+  }
+
+
+  @Test
+  public void testInferOrderTaskStatusOnSafeOrderfree() throws Exception {
+    //TODO
+    String orderNo = "E123";
+    Integer kdtId= 1;
+    Safe safe = new Safe();
+    safe.setOrderNo(orderNo);
+    safe.setKdtId(kdtId);
+    when(safeService.checkOrderFeedbackFinish(orderNo, kdtId)).thenReturn(Boolean.TRUE);
+    Map<Integer,TaskStatus> datas = inferOrderTaskStatusOnSafeTestDataWithOrderSafeFree();
+    for(Map.Entry<Integer,TaskStatus> entry:datas.entrySet()){
+      safe.setState(entry.getKey());
+      assertEquals(entry.getValue(),dispatchingOrderTaskOnSafeProcessor.inferOrderTaskStatusOnSafe(safe));
+    }
+  }
+
+  public Map<Integer,TaskStatus> inferOrderTaskStatusOnSafeTestDataWithOrderSafeFree(){
+    Map<Integer,TaskStatus> datas = Maps.newHashMap();
+    //没有其他维权进行中
+    datas.put(SafeState.BUYER_START.code(),TaskStatus.SUSPENDED);
+    datas.put(SafeState.CLOSED.code(),TaskStatus.ACTIVE);
+    datas.put(SafeState.FINISHED.code(),TaskStatus.ACTIVE);
+    datas.put(SafeState.BUYER_RESTART.code(),null);
+    datas.put(SafeState.BUYER_SENT.code(),null);
+    datas.put(SafeState.INVOLVED.code(),null);
+    datas.put(SafeState.SELLER_ACCEPTED.code(),null);
+    datas.put(SafeState.SELLER_NOT_RECEIVED.code(),null);
+    datas.put(SafeState.SELLER_REJECTED.code(),null);
+    return datas;
+  }
+
+  public Map<Integer,TaskStatus> inferOrderTaskStatusOnSafeTestDataWithOrderSafeUnFree(){
+    //其他维权进行中
+    Map<Integer,TaskStatus> datas = Maps.newHashMap();
+    datas.put(SafeState.BUYER_START.code(),TaskStatus.SUSPENDED);
+    datas.put(SafeState.CLOSED.code(),null);
+    datas.put(SafeState.FINISHED.code(),null);
+    datas.put(SafeState.BUYER_RESTART.code(),null);
+    datas.put(SafeState.BUYER_SENT.code(),null);
+    datas.put(SafeState.INVOLVED.code(),null);
+    datas.put(SafeState.SELLER_ACCEPTED.code(),null);
+    datas.put(SafeState.SELLER_NOT_RECEIVED.code(),null);
+    datas.put(SafeState.SELLER_REJECTED.code(),null);
+    return datas;
+
   }
 }
