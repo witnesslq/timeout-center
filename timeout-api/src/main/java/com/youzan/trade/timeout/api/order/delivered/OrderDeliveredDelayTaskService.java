c3d9200d782114f07208a041cbdef538f6131beb
apple <zhengchen@youzan.com>
2016-01-14 11:44:11 +0800

    加入api模块

---
+++ timeout-api/src/main/java/com/youzan/trade/timeout/api/order/delivered/OrderDeliveredDelayTaskService.java
@@ -0,0 +1,10 @@
+package com.youzan.trade.timeout.api.order.delivered;
+
+/**
+ * 针对订单已发货业务, 对外提供服务
+ *
+ * @author apple created at: 16/1/14 上午11:12
+ */
+public interface OrderDeliveredDelayTaskService {
+
+}
---
+++ timeout-service/src/main/java/com/youzan/trade/timeout/service/impl/OrderDeliveredDelayTaskServiceImpl.java
@@ -0,0 +1,10 @@
+package com.youzan.trade.timeout.service.impl;
+
+import com.youzan.trade.timeout.api.order.delivered.OrderDeliveredDelayTaskService;
+
+/**
+ * @author apple created at: 16/1/14 上午11:39
+ */
+public class OrderDeliveredDelayTaskServiceImpl implements OrderDeliveredDelayTaskService {
+
+}
