dcee3c123482d17b3f584b182f30b5461c76d4e4
liwenjia <liwenjia@youzan.com>
2016-01-18 22:29:03 +0800

    补充错误码

--- timeout-service/src/main/java/com/youzan/trade/timeout/constants/ErrorCode.java
+++ timeout-service/src/main/java/com/youzan/trade/timeout/constants/ErrorCode.java
@@ -9,6 +9,6 @@ public class ErrorCode {
 
   public static final String ORDER_IN_ABNORMAL_STATE = "412";
 
-  public static final String TASK_NOT_FOUND = "";
+  public static final String TASK_NOT_FOUND = "404";
 
 }
